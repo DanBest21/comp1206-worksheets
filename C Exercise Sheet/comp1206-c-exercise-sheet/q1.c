7ae816e6b36cc0799a7d70501c18107cfc9eb41c
db5n17 <db5n17@soton.ac.uk>
2019-05-15 01:43:58 +0100

    Finished initial work.

--- C Exercise Sheet/comp1206-c-exercise-sheet/q1.c
+++ C Exercise Sheet/comp1206-c-exercise-sheet/q1.c
@@ -7,7 +7,7 @@ int analyse(double *prod, double *sum, double d1, double d2)
 {
 	int i1, i2;
 
-	// Round the two passed double values to the last integer.
+	// Round the two passed double values to the nearest integer that is not greater than it.
 	i1 = floor(d1);
 	i2 = floor(d2);
 
@@ -15,12 +15,12 @@ int analyse(double *prod, double *sum, double d1, double d2)
 	*prod = i1 * i2;
 	*sum = i1 + i2;
 
-	// If the signs are different, or one value is equal to zero but the other is not, return -1.
-	if ((signbit(d1) == 0 && signbit(d2) != 0) || (signbit(d1) == 0 && signbit(d2) != 0) || (d1 == 0 && d1 != d2) || (d1 != d2 && d2 == 0))
+	// If the signs are the same (the product is positive) or both values are zero, return 1.
+	if ((d1 * d2) > 0 || (d1 == 0 && d1 == d2))
 	{
-		return -1;
+		return 1;
 	}
 	
-	// Otherwise return 1.
-	return 1;
+	// Otherwise return -1.
+	return -1;
 }
--- C Exercise Sheet/comp1206-c-exercise-sheet/q2.c
+++ C Exercise Sheet/comp1206-c-exercise-sheet/q2.c
@@ -8,43 +8,33 @@ int checkFETCH(CHECKARR as, int N, int row, int col);
 
 CHECKARR checkNew(int N)
 {
+	// Return a pointer to a block of memory that is N * N * int (4 bits) worth of memory - i.e. enough for N * N ints.
 	return (int*)malloc(sizeof(int) * (N * N));
 }
 
 int checkSTORE(CHECKARR as, int N, int row, int col, int val)
 {
+	// If the parity of row and col is the same (they have the same result from modulo 2), and they are both within range of the array:
 	if ((row % 2 == col % 2) && (row < N && col < N) && (row >= 0 && col >= 0))
 	{
+		// Set the passed value to the correct element, which is determined by (N * row) + col and return 1 to indicate success.
 		as[(N * row) + col] = val;
 		return 1;
 	}
 	
+	// Otherwise return -1 and don't set the value as it is at an invalid position.
 	return -1;
 }
 
 int checkFETCH(CHECKARR as, int N, int row, int col)
 {
+	// If the parity of row and col is the same, and they are both within range of the array:
 	if ((row % 2 == col % 2) && (row < N && col < N) && (row >= 0 && col >= 0))
 	{
+		// Return the corresponding element.
 		return as[(N * row) + col];
 	}
 
+	// Otherwise return -1 to indicate the value cannot be fetched.
 	return -1;
-}
-
-int main(void)
-{
-	const int N = 6;
-	CHECKARR arr = checkNew(N);
-
-	printf("checkSTORE 1: %i\n", checkSTORE(arr, N, 2, 2, 56));
-	printf("checkSTORE 2: %i\n", checkSTORE(arr, N, 3, 5, 112));
-	printf("checkSTORE 3: %i\n", checkSTORE(arr, N, 0, 0, 5667));
-	printf("checkSTORE 4: %i\n", checkSTORE(arr, N, 0, 1, 23));
-	printf("checkSTORE 5: %i\n", checkSTORE(arr, N, 2, 5, 234));
-	printf("checkSTORE 6: %i\n", checkSTORE(arr, N, 7, 5, 1112));
-
-	printf("checkFETCH 1: %i\n", checkFETCH(arr, N, 3, 5));
-	printf("checkFETCH 2: %i\n", checkFETCH(arr, N, 0, 1));
-	printf("checkFETCH 3: %i\n", checkFETCH(arr, N, 10, 10));
 }
