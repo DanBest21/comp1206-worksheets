bbd9c38a2e26d398f5d5e3eebbe7aa5ca0a41d15
db5n17 <db5n17@soton.ac.uk>
2019-05-15 15:16:03 +0100

    Fixed q2.c issue.

--- C Exercise Sheet/comp1206-c-exercise-sheet/q2.c
+++ C Exercise Sheet/comp1206-c-exercise-sheet/q2.c
@@ -2,21 +2,21 @@
 
 typedef int* CHECKARR;
 
-CHECKARR checkNew(int N);
+CHECKARR checkNEW(int N);
 int checkSTORE(CHECKARR as, int N, int row, int col, int val);
 int checkFETCH(CHECKARR as, int N, int row, int col);
 
-CHECKARR checkNew(int N)
+CHECKARR checkNEW(int N)
 {
 	// Return a pointer to a block of memory that is N * N * int (4 bits) worth of memory - i.e. enough for N * N ints.
-	return (int*)malloc(sizeof(int) * (N * N));
+	return malloc(sizeof(int) * (N * N));
 }
 
 int checkSTORE(CHECKARR as, int N, int row, int col, int val)
 {
 	// If the parity of row and col is the same (they have the same result from modulo 2), and they are both within range of the array:
 	if ((row % 2 == col % 2) && (row < N && col < N) && (row >= 0 && col >= 0))
-	{
+	{		
 		// Set the passed value to the correct element, which is determined by (N * row) + col and return 1 to indicate success.
 		as[(N * row) + col] = val;
 		return 1;
@@ -30,7 +30,7 @@ int checkFETCH(CHECKARR as, int N, int row, int col)
 {
 	// If the parity of row and col is the same, and they are both within range of the array:
 	if ((row % 2 == col % 2) && (row < N && col < N) && (row >= 0 && col >= 0))
-	{
+	{		
 		// Return the corresponding element.
 		return as[(N * row) + col];
 	}
